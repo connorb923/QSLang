08337fcf9e39893b479adf0ef9a3e2d197c8037d
Connor B. <connorbieszk923@gmail.com>
2024-11-13 21:39:41 -0600

    Create Transpiler.lua

---
+++ Transpiler.lua
@@ -0,0 +1,18 @@
+--[[
+  This is to be used in the ROBLOX™ Engine as a Module Script in Server script service
+  Code is not ready for production use!
+]]
+
+local Transpiler = {}
+
+function Transpiler.TranspileQuickScript(QSCode)
+	local luau = ""
+	
+	local lineDictionary = string.split(QSCode, ";")
+	
+	
+	
+	return luau
+end
+
+return Transpiler
